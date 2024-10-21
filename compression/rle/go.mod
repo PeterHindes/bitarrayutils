ee333e5243fd0f4613b901487cc72563365febdb
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2024-10-20 23:34:48 -0600

    oops

---
+++ compression/rle/go.mod
@@ -0,0 +1,3 @@
+module github.com/PeterHindes/bitarrayutils/compression/rle
+
+go 1.23.0
---
+++ compression/rle/rle.go
@@ -0,0 +1,9 @@
+package rle
+
+import (
+	"math"
+)
+
+
+// run length encode 
+func RunLengthEncode(array []bool, powerOfTwo int) []bool {
